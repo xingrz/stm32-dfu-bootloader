caa5a7da2c7139a6a8046e5987834c4c7a565a41
XiNGRZ <hi@xingrz.me>
2022-08-18 13:28:54 +0800

    Disable protections for generic target

--- targets/generic/target.cmake
+++ targets/generic/target.cmake
@@ -1,4 +1,4 @@
-target_compile_definitions(${PROJECT_NAME} PRIVATE ENABLE_PROTECTIONS)
+# target_compile_definitions(${PROJECT_NAME} PRIVATE ENABLE_PROTECTIONS)
 target_compile_definitions(${PROJECT_NAME} PRIVATE ENABLE_CHECKSUM)
 target_compile_definitions(${PROJECT_NAME} PRIVATE ENABLE_SAFEWRITE)
 target_compile_definitions(${PROJECT_NAME} PRIVATE ENABLE_WATCHDOG=20)
